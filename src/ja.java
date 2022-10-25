a1481b0be6cc16a6b8d4b251b0a0d1eaf16a3c2f
zed-o-z <zeddeju@gmail.com>
2022-10-25 20:55:15 +0300

    java op

---
+++ src/ja.java
@@ -0,0 +1,32 @@
+public class ja {
+    public static void main(String args[]) {
+  //      int x = 10;
+    //    System.out.println(x++);//10 (11)
+      //  System.out.println(++x);//12
+        //System.out.println(x--);//12 (11)
+   //     Sstem.out.println(--x);//10
+
+
+     //   int a = 10;
+   //     int b = 5;
+       // System.out.println(a + b);//15
+        //System.out.println(a - b);//5
+        //System.out.println(a * b);//50
+        //System.out.println(a / b);//2
+        //System.out.println(a%b);//0
+//int fnumber=8;
+//int snumber=15;
+//boolean value = fnumber==snumber;
+//system.out.printly(boolean)
+
+    boolean criminal=false;
+    boolean banksave =true;
+    boolean salary=true;
+    boolean score=false;
+    boolean credit=(criminal&&( salary || score));
+    boolean credit=(score||(score %% banksave));
+sytem.out.printly(credit);
+
+
+    }
+}
